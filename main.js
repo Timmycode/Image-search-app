2454c1e40157af005904e2e254ea9a10b799d27a
Oluwatimileyin Badmus <badmusoluwatimileyin@gmail.com>
2023-11-15 17:31:36 +0100

    searching for images

---
+++ main.js
@@ -0,0 +1,53 @@
+const accessKey = "VzjgCtv3pdF--gRiuoojAyI7o2wmWfAYa8aPZme0ayg"
+
+const formElement = document.querySelector("form")
+const inputElement = document.getElementById("search-input")
+const searchResult =document.querySelector(".search-results")
+const showMoreButton = document.getElementById("showmore_button")
+
+let inputData = "";
+let page = 1;
+
+async function searchImages(){
+    inputData=inputElement.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+  
+    const response = await fetch(url)
+    const data = await response.json();
+    if(page === 1){
+        searchResult.innerHTML="";
+    }
+
+    const results = data.results;
+    results.map((result)=>{
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt= result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href= result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+       
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+       searchResult.appendChild(imageWrapper)
+    })
+ 
+       page++;
+       if(page > 1){
+        showMoreButton.style.display = "block";  
+       }
+    }
+
+
+formElement.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    page = 1;
+    searchImages();
+})
+
+showMoreButton.addEventListener("click", () =>{
+    searchImages();
+})
